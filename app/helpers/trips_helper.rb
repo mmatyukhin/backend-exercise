cc87b77f745bcdd4a8d86a31b0e5518823f5fecb
Mikhail Matyukhin <m.facepalm@gmail.com>
2019-04-12 00:46:20 +0300

    simple schedule for trips added

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -1,2 +1,11 @@
 module TripsHelper
+DAYS_OF_THE_WEEK = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']
+
+def days(trip)
+  if trip.days_of_the_week.size == 7
+    return 'Daily'
+  else
+    trip.days_of_the_week.map { |day| DAYS_OF_THE_WEEK[day] }.join(' ')
+  end
+end
 end
