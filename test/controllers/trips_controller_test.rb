1c8b34bf2ed1673f2500354147771562ed6c202d
Mikhail Matyukhin <m.facepalm@gmail.com>
2019-04-15 10:43:53 +0300

    test for trip controller updated

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class TripsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @trip = trips(:one)
+    @trip = trips(:trip)
   end
 
   test "should get index" do
@@ -17,7 +17,7 @@ class TripsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create trip" do
     assert_difference('Trip.count') do
-      post trips_url, params: { trip: {  } }
+      post trips_url, params: { trip: { carrier_name: @trip.carrier_name, currency: @trip.currency, end_city_name: @trip.end_city_name, end_time: @trip.end_time, start_city_name: @trip.start_city_name, start_time: @trip.start_time, station_begin_name: @trip.station_begin_name, station_end_name: @trip.station_end_name, total_cost: @trip.total_cost } }
     end
 
     assert_redirected_to trip_url(Trip.last)
@@ -34,7 +34,7 @@ class TripsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update trip" do
-    patch trip_url(@trip), params: { trip: {  } }
+    patch trip_url(@trip), params: { trip: { carrier_name: @trip.carrier_name, currency: @trip.currency, end_city_name: @trip.end_city_name, end_time: @trip.end_time, start_city_name: @trip.start_city_name, start_time: @trip.start_time, station_begin_name: @trip.station_begin_name, station_end_name: @trip.station_end_name, total_cost: @trip.total_cost } }
     assert_redirected_to trip_url(@trip)
   end
 
